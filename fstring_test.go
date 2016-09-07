f4b91a306785ef88e9e0e7d70b2d00e891722077
ieee0824 <ieee0824@ezweb.ne.jp>
2016-09-07 18:00:17 +0900

    add test

---
+++ fstring_test.go
@@ -0,0 +1,92 @@
+package fstring
+
+import (
+	"testing"
+)
+
+func TestAddNormalString(t *testing.T) {
+	str0 := "hoge"
+	str1 := "huga"
+
+	fs := New(str0)
+
+	if err := fs.Add(str1); err != nil {
+		t.Fatal(err)
+	}
+
+	if fs.String() != str0+str1 {
+		t.Fatal(fs.String(), ":", str0+str1)
+	}
+}
+
+func TestAddFastString(t *testing.T) {
+	str0 := "hoge"
+	str1 := "huga"
+
+	fs := New(str0)
+
+	if err := fs.Add(New(str1)); err != nil {
+		t.Fatal(err)
+	}
+
+	if fs.String() != str0+str1 {
+		t.Fatal(fs.String(), ":", str0+str1)
+	}
+}
+
+func TestAddBlankString00(t *testing.T) {
+	str0 := ""
+	str1 := ""
+
+	fs := New(str0)
+
+	if err := fs.Add(New(str1)); err != nil {
+		t.Fatal(err)
+	}
+
+	if fs.String() != str0+str1 {
+		t.Fatal(fs.String(), ":", str0+str1)
+	}
+}
+
+func TestAddBlankString01(t *testing.T) {
+	str0 := "hoge"
+	str1 := ""
+
+	fs := New(str0)
+
+	if err := fs.Add(New(str1)); err != nil {
+		t.Fatal(err)
+	}
+
+	if fs.String() != str0+str1 {
+		t.Fatal(fs.String(), ":", str0+str1)
+	}
+}
+
+func TestAddBlankString02(t *testing.T) {
+	str0 := ""
+	str1 := "hoge"
+
+	fs := New(str0)
+
+	if err := fs.Add(New(str1)); err != nil {
+		t.Fatal(err)
+	}
+
+	if fs.String() != str0+str1 {
+		t.Fatal(fs.String(), ":", str0+str1)
+	}
+}
+
+func TestAddNotString(t *testing.T) {
+	str0 := "huga"
+	str1 := 10
+
+	fs := New(str0)
+
+	err := fs.Add(str1)
+	if err == nil {
+		t.Fatal(err)
+	}
+}
